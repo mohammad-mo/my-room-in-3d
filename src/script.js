76174bfd309ef59b8f379bfacb9ed057eec0570e
Mohammad <mohammadmahone@gmail.com>
2021-09-17 14:14:48 +0430

    loading manager added.

--- src/script.js
+++ src/script.js
@@ -9,6 +9,7 @@ import mirrorFragmentShader from './shaders/mirror/fragment.glsl'
 import coffeSteamVertexShader from './shaders/coffeSteam/vertex.glsl'
 import coffeSteamFragmentShader from './shaders/coffeSteam/fragment.glsl'
 import { Raycaster, Vector2 } from 'three'
+import { gsap } from 'gsap'
 
 let playButton = document.getElementById('playButton')
 let pauseButton = document.getElementById('pauseButton')
@@ -17,6 +18,7 @@ let point1 = document.getElementById('point-1')
 let point2 = document.getElementById('point-2')
 let text = document.getElementById('text')
 let text1 = document.getElementById('text1')
+const loadingBarElement = document.querySelector('.loading-bar')
 
 "mousemove click".split(" ").forEach((e) =>
 {
@@ -69,7 +71,7 @@ point2.addEventListener('click', () =>
 
 /**
  * Base
- */
+ */ 
 // Debug
 // const gui = new dat.GUI({
 //     width: 400
@@ -82,13 +84,59 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Loaders
- */
+* Loaders
+*/
+const loadingManager = new THREE.LoadingManager(
+   // Loaded
+   () =>
+   {
+      gsap.delayedCall(0.5, () =>
+      {
+            gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0, delay: 1 })
+            loadingBarElement.classList.add('ended')
+            loadingBarElement.style.transform = ''
+      })
+   },
+   
+   // Progress
+   (itemUrl, itemsLoaded, itemsTotal) =>
+   {
+       const progressRatio = itemsLoaded / itemsTotal
+       loadingBarElement.style.transform = `scaleX(${progressRatio})`
+   }
+)
 // Texture loader
-const textureLoader = new THREE.TextureLoader()
+const textureLoader = new THREE.TextureLoader(loadingManager)
 
 // GLTF loader
-const gltfLoader = new GLTFLoader()
+const gltfLoader = new GLTFLoader(loadingManager)
+
+/**
+* Overlay
+*/
+const overlayGeometry = new THREE.PlaneBufferGeometry(2, 2, 1, 1)
+const overlayMaterial = new THREE.ShaderMaterial({
+    uniforms:
+    {
+        uAlpha: { value: 1}
+    },
+    transparent: true,
+    vertexShader: `
+        void main()
+        {
+            gl_Position = vec4(position, 1);
+        }
+    `,
+    fragmentShader: `
+        uniform float uAlpha;
+        void main()
+        {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+        }
+    `
+})
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
+scene.add(overlay)
 
 // panel material
 const panelMaterial = new THREE.ShaderMaterial({
@@ -229,6 +277,19 @@ gltfLoader.load(
     }
 )
 
+// const loader = [scene, bakedTexture, bakedTexture1, bakedTexture2, bakedTexture3, bakedTexture4]
+// const loadingManager = new THREE.LoadingManager(gltf)
+// loadingManager.onProgress = (url, itemsLoaded, itemsTotal) =>
+// {
+//     loader.textContent = `${Math.round(itemsLoaded / itemsTotal * 100)}% Loading...`
+//     if(itemsLoaded === itemsTotal)
+//     {
+//         setTimeout(() =>
+//         {
+//             loader.style.opacity = 0
+//         }, 2000)
+//     }
+// }
 
 /**
  * Fireflies
