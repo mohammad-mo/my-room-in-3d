58ea25c35bee49ffbadda11b2f47f59a7d088a72
Mohammad <mohammadmahone@gmail.com>
2021-12-11 12:02:22 +0330

    just playing around with numbers

--- src/shaders/mirror/fragment.glsl
+++ src/shaders/mirror/fragment.glsl
@@ -14,24 +14,26 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     float uTime = uTime * 0.04; 
 
-    vec2 p = mod(vUv * PI2, PI2) - 100.0;
+    vec2 p = mod(vUv * PI2, PI2) - 150.0;
     vec2 i = vec2(p);
     float c = 0.5;
     float inten =  0.0094;
 
     for (int n = 0; n < MAX_ITER; n++) {
         float t = uTime * (4.5 - (2.2 / float(n + 122)));
-        i = p + vec2(sin((t - i.x) + cos(t + i.y)) * 4.0, cos((t - i.y) + sin(t + i.x)) * 5.0);
-        c += 1.0 / length(vec2(p.x / (sin(i.x + t) / inten), p.y / (cos(i.y + t) / inten)));
+        i = p + vec2(sin((t - i.x * 0.5) + cos(t - i.y / 0.8)) * 5.0, cos((t - i.y * 0.8) + cos(t + i.x / 0.5)) * 2.0) * 0.8;
+        c += 1.0 / length(vec2(p.x / (sin(i.x - t) / inten), p.y / (cos(i.y + t) / inten)));
     }
 
     c /= float(MAX_ITER);
-    c = 1.10 - pow(c, 1.26);
-    vec3 color = mix(uColorStart * 0.2, uColorEnd / 1.2, pow(abs(c), 4.1));
+    c = 1.20 - pow(c, 1.3);
+    vec3 color = mix(uColorStart / 3.5, uColorEnd / 2.3, pow(abs(c), 15.0));
 
-    fragColor = vec4(color, 1.3);
+    fragColor = vec4(color, 1.0);
 }
 
 
-void main( void ) {
-    mainImage(gl_FragColor, gl_FragCoord.xy);}
+void main( void ) 
+{
+    mainImage(gl_FragColor, gl_FragCoord.xy);
+}
